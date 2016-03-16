dd0f64b69946eb0a30cf3ce48ab531dc34b12a25
oscartang0629 <oscartang2008@gmail.com>
2016-03-15 20:18:11 -0700

    New view controller

--- selfigram/ViewController.swift
+++ selfigram/ViewController.swift
@@ -8,10 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBAction func camerabuttonpressed(sender: AnyObject) {
+        
+        // 1: Create an ImagePickerController
+        let pickerController = UIImagePickerController()
+        
+        // 2: Self in this line refers to this View Controller
+        //    Setting the Delegate Property means you want to receive a message
+        //    from pickerController when a specific event is triggered.
+        pickerController.delegate = self
+        
+        if TARGET_OS_SIMULATOR == 1 {
+            // 3. We check if we are running on a Simulator
+            //    If so, we pick a photo from the simulator’s Photo Library
+            // We need to do this because the simulator does not have a camera
+            pickerController.sourceType = .PhotoLibrary
+        } else {
+            // 4. We check if we are running on an iPhone or iPad (ie: not a simulator)
+            //    If so, we open up the pickerController's Camera (Front Camera, for selfies!)
+            pickerController.sourceType = .Camera
+            pickerController.cameraDevice = .Front
+            pickerController.cameraCaptureMode = .Photo
+        }
+        
+        // Preset the pickerController on screen
+        self.presentViewController(pickerController, animated: true, completion: nil)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        usernameLabel.text = "Oscar"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +52,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
